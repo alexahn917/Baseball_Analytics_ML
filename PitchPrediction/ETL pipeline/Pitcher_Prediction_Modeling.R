1057a35aaed7176485d07bb149f6f214fead17ce
Alex Ahn <alexahn917@gmail.com>
2017-03-27 10:11:34 -0400

    Phase 2: data mining / feature extension
    Collapsed model_code repository

--- PitchPrediction/ETL pipeline/Pitcher_Prediction_Modeling.R
+++ PitchPrediction/ETL pipeline/Pitcher_Prediction_Modeling.R
@@ -5,23 +5,11 @@ library(xtable)
 library(data.table)
 library(tidyr)
 
-db <- src_sqlite('~/Documents/Github/DB/pitchRx_14_16.sqlite3')
-
-# get pitchers list
-pitchers = read.table("../pitchers.txt", 
-               sep="\n",
-               fill=FALSE,
-               col.names= "target_pitcher",
-               strip.white=TRUE)
-
-# iterate over pitchers
-for (pitcher in pitchers$target_pitcher) 
+extract_pitches <- function(target_pitcher, db)
 {
-  extract_pitches(pitcher)
-}
-
-extract_pitches <- function(target_pitcher)
-{
+  db <- src_sqlite('~/Documents/Github/DB/pitchRx_14_16.sqlite3')
+  target_pitcher = "Clayton Kershaw"
+  
   # Join the location and names table into a new que table.
   pitch <- dbGetQuery(db$con, 'SELECT pitch_type, inning, count, on_1b, on_2b, on_3b, type_confidence,
                       num, gameday_link FROM pitch')
@@ -32,6 +20,10 @@ extract_pitches <- function(target_pitcher)
   games <- dbGetQuery(db$con, 'SELECT gameday_link, home_team_id FROM game')
   games$gameday_link <- paste('gid_',games$gameday_link, sep="")
   
+  #batter_stats <- (db$con, 'SELECT ')
+  
+  #pitcher_stats
+
   que <- inner_join(pitch, filter(names, pitcher_name == target_pitcher),
                        by = c('num', 'gameday_link'))
   
@@ -110,6 +102,12 @@ extract_pitches <- function(target_pitcher)
   data[data$balls == 0 & data$strikes == 0, ]$prev_pitch_type <- -1
   data <- data[!is.na(data$prev_pitch_type),]
   
+  # retrieve 2 previous pitch ball type
+  prevprev_pitch_type <- lag(data$prev_pitch_type, 1)
+  data$prevprev_pitch_type <- prevprev_pitch_type
+  data[(data$balls == 0 & data$strikes == 0) | (data$balls == 1 & data$strikes == 0) | (data$balls == 0 & data$strikes == 1), ]$prevprev_pitch_type <- -1
+  data <- data[!is.na(data$prevprev_pitch_type),]
+    
   # Drop NA pitch_type
   data <- data[!is.na(data$pitch_type),]
   
@@ -133,123 +131,18 @@ extract_pitches <- function(target_pitcher)
   data <- data[data$type_confidence > 0.90,]
   data <- data[, !names(data) %in% "type_confidence"]
   
-  write.csv(data, file=paste("csv_data/",target_pitcher,".csv", sep=""), row.names = FALSE)
+  # extract specific domain
+  #data <- data[data$on_3b==1, ]
+
+  randomized_data <- data[sample(nrow(data)),]
+  data_len <- length(randomized_data$pitch_type)
+  split_size <- ceiling((3/4)*data_len)
+  train_data <- randomized_data[1:split_size, ]
+  test_data <- randomized_data[(split_size+1):data_len, ]
+
+  write.csv(train_data, file=paste("CSV/extended/",target_pitcher,"_train.csv", sep=""), row.names = FALSE)
+  write.csv(test_data, file=paste("CSV/extended/",target_pitcher,"_test.csv", sep=""), row.names = FALSE)
 }
-
-#dbListFields(db$con, "atbat")
-
-#dbListFields(db$con, "atbat")
-
-# Join the location and names table into a new que table.
-pitch <- dbGetQuery(db$con, 'SELECT pitch_type, inning, count, on_1b, on_2b, on_3b, type_confidence,
-                    num, gameday_link FROM pitch')
-
-names <- dbGetQuery(db$con, 'SELECT date, pitcher AS pitcher_id, pitcher_name, batter AS batter_id, 
-                    batter_name, num, b_height, gameday_link, home_team_runs, away_team_runs, o AS out, p_throws as pitch_rl, stand as bat_rl FROM atbat') #stand?
-
-games <- dbGetQuery(db$con, 'SELECT gameday_link, home_team_id FROM game')
-games$gameday_link <- paste('gid_',games$gameday_link, sep="")
-
-que <- inner_join(pitch, filter(names, pitcher_name == target_pitcher),
-                     by = c('num', 'gameday_link'))
-
-que <- inner_join(que, games, by = c('gameday_link'))
-
-pitchfx <- as.data.frame(collect(que))
-pitchfx <- data.table(pitchfx[ do.call(order, pitchfx[ , c('gameday_link','inning', 'num') ] ), ])
-pitchfx[, batter_num:=as.numeric(factor(num)), by=gameday_link]
-pitchfx <- as.data.frame(pitchfx)
-
-# Create pitcher_at_home field
-pitcher_id <- as.numeric(pitchfx$pitcher_id[1])
-sqlStatement <- paste('SELECT team_id as pitcher_team_id FROM player WHERE id = ', pitcher_id, 'LIMIT 1')
-pitcher_team_id <- as.numeric(dbGetQuery(db$con, sqlStatement))
-pitchfx$pitcher_at_home[pitchfx$home_team_id == pitcher_team_id] <- 1
-pitchfx$pitcher_at_home[pitchfx$home_team_id != pitcher_team_id] <- -1
-
-# Create a new field for the batting order number.
-pitchfx$batter_num <- ifelse(pitchfx$batter_num %% 9 == 0, 9, (pitchfx$batter_num %% 9))
-pitchfx$batter_num <- as.factor(pitchfx$batter_num)
-pitchfx$pitch_type <- as.factor(pitchfx$pitch_type)
-
-# Get # of outs
-pitchfx$out <- pitchfx$out - 1
-pitchfx$out[pitchfx$out == -1] <- 0
-
-# compute score difference
-pitchfx$score_diff <- (as.numeric(pitchfx$home_team_runs) - as.numeric(pitchfx$away_team_runs)) * pitchfx$pitcher_at_home
-pitchfx <- pitchfx[!is.na(pitchfx$score_diff),]
-
-# Cleaning up data df.
-data <- as.data.frame(select(pitchfx, type_confidence, pitch_type, batter_num, pitch_rl, bat_rl, inning,
-                             count, out, on_1b, on_2b, on_3b, score_diff, pitcher_at_home))
-
-#pitcher$uniqueID <- paste(pitcher$num, pitcher$gameday_link, pitcher$inning, sep='')
-
-# convert handedness boolean (0|1)
-data$pitch_rl[data$pitch_rl == 'L'] <- 0
-data$pitch_rl[data$pitch_rl == 'R'] <- 1
-data$bat_rl[data$bat_rl == 'L'] <- 0
-data$bat_rl[data$bat_rl == 'R'] <- 1
-
-#splot balls / strike counts into two columns
-data <- separate(data = data, col = count, into = c("balls", "strikes"), sep = "\\-")
-
-# convert on_bases IDs to boolean (0|1)
-data$on_1b <- replace(data$on_1b, is.na(data$on_1b), 0)
-data$on_1b <- replace(data$on_1b, data$on_1b != 0, 1)
-data$on_2b <- replace(data$on_2b, is.na(data$on_2b), 0)
-data$on_2b <- replace(data$on_2b, data$on_2b != 0, 1)
-data$on_3b <- replace(data$on_3b, is.na(data$on_3b), 0)
-data$on_3b <- replace(data$on_3b, data$on_3b != 0, 1)
-
-# convert pitch ball types into integer classes
-data$pitch_type <- as.character(data$pitch_type)
-data$pitch_type[data$pitch_type == 'FA'] <- 0
-data$pitch_type[data$pitch_type == 'FF'] <- 1
-data$pitch_type[data$pitch_type == 'FT'] <- 2
-data$pitch_type[data$pitch_type == 'FC'] <- 3
-data$pitch_type[data$pitch_type == 'FS'] <- 4
-data$pitch_type[data$pitch_type == 'SI'] <- 5
-data$pitch_type[data$pitch_type == 'SF'] <- 6
-data$pitch_type[data$pitch_type == 'SL'] <- 7
-data$pitch_type[data$pitch_type == 'CH'] <- 8
-data$pitch_type[data$pitch_type == 'CB'] <- 9
-data$pitch_type[data$pitch_type == 'CU'] <- 10
-data$pitch_type[data$pitch_type == 'KC'] <- 11
-data$pitch_type[data$pitch_type == 'KN'] <- 12
-data$pitch_type[data$pitch_type == 'EP'] <- 13
-data$pitch_type[data$pitch_type == 'IN'] <- 14
-
-# retrieve previous pitch ball type
-prev_pitch_type <- lag(data$pitch_type, 1)
-data$prev_pitch_type <- prev_pitch_type
-data[data$balls == 0 & data$strikes == 0, ]$prev_pitch_type <- -1
-data <- data[!is.na(data$prev_pitch_type),]
-
-# drop 'NA's under pitch_type
-data <- data[!is.na(data$pitch_type),]
-
-# select only the common/frequent pitch types ( > 10%)
-pitch_labels <- levels(as.factor(data$pitch_type))
-pitch_type_props <- rep(0,length(pitch_labels))
-i=1
-for (PT in pitch_labels) {
-  pitch_type_props[i] = length(data$pitch_type[data$pitch_type==PT]) / length(data$pitch_type)
-  i = i+1;
-}
-pitch_type_props <- melt(data.frame(pitch_labels, pitch_type_props))
-pitch_type_props <- pitch_type_props[order(-pitch_type_props$value),]
-
-# drop levels that are not useful (Under proportions of 0.10)
-used_pitch_types <- pitch_type_props$pitch_labels[pitch_type_props$value > 0.10]
-data <- data[data$pitch_type %in% used_pitch_types,]
-
-# only use instances where type confidence is at least 0.90
-data <- data[data$type_confidence > 0.90,]
-data <- data[, !names(data) %in% "type_confidence"]
-
-write.csv(data, file=paste("ETL pipeline/raw_data/",target_pitcher,".csv", sep=""), row.names = FALSE)
 
 # ----------------FEATURES----------------------
 # Pitch Type: [pitch_type(char)]
@@ -258,3 +151,20 @@ write.csv(data, file=paste("ETL pipeline/raw_data/",target_pitcher,".csv", sep="
 # Game: [Innings(#), balls(#), strikes(#), on_1b(#), on_2b(#), on_3b(#), score_differencial(#)]
 
 # Data: [pitch_type, prev_pitch_type, batter_num, pitch_rl, bat_rl, inning, balls, strikes, on_1b, on_2b, on_3b, score_diff
+
+# get pitchers list
+
+
+db <- src_sqlite('~/Documents/Github/DB/pitchRx_14_16.sqlite3')
+
+pitchers = read.table("../pitchers.txt", 
+                      sep="\n",
+                      fill=FALSE,
+                      col.names= "target_pitcher",
+                      strip.white=TRUE)
+
+# iterate over pitchers
+for (pitcher in pitchers$target_pitcher) 
+{
+  extract_pitches(pitcher, db)
+}
--- PitchPrediction/model_scikit-learn/run.py
+++ PitchPrediction/model_scikit-learn/run.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import csv
 import pickle
 from sklearn import datasets, svm, metrics
@@ -13,25 +14,24 @@ from scipy.stats import randint as sp_randint
 from sklearn.externals import joblib
 from sklearn.model_selection import cross_val_score
 
+
 def main():
     with open("../pitchers.txt", "r") as f:
       names = f.read().split('\n')
       print(names)
+    for pitcher_name in names:
+      train_data = readCSV(pitcher_name, 'train')
+      test_data = readCSV(pitcher_name, 'test')      
+      train(train_data, pitcher_name)
+      predict(test_data, pitcher_name)
 
-#    for pitcher_name in names:
-#      data = readCSV(pitcher_name)
-#      train(data, pitcher_name)
-#      predict(data, pitcher_name)
-    data = readCSV("Jake Arrieta")
-    train(data, "Jake Arrieta")
 
-def readCSV(pitcher_name):
-    csv_file = '../ETL pipeline/csv_data/' + pitcher_name + '.csv'
+def readCSV(pitcher_name, file_type):
+    csv_file = '../ETL pipeline/CSV/extended/' + pitcher_name + '_' + file_type +'.csv'
     file = open(csv_file, "r")
     reader = csv.reader(file)
     instances = []
     target = []
-
     row_num = 0 
     for row in reader:
         if row_num is 0:
@@ -51,53 +51,66 @@ def readCSV(pitcher_name):
     data = [instances, target]
     return data
 
+
 def train(data, pitcher_name):
     X = data[0]
     y = data[1]
     n_samples = len(X)
 
-    # split into a training and testing set
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
-
 #    clf = OneVsRestClassifier(LinearSVC(random_state=0))
 #    clf = OneVsOneClassifier(LinearSVC(random_state=0))
 #    clf = svm.SVC(decision_function_shape='ovr')
 #    clf = GridSearchCV(svm.SVC(kernel='rbf', decision_function_shape='ovr'), param_grid)
-#    clf = MLPClassifier()
+    clf = MLPClassifier()
+#    clf = RandomForestClassifier()
 
-    clf = RandomForestClassifier()
-#    clf.fit(X_train, y_train)
-#    eval(clf, y_test, clf.predict(X_test))#
-
-    # run grid search
-    param_grid = {'n_estimators': [10, 500],
-                  'max_features': ['auto', 'sqrt', 'log2'],
-                  "max_depth": [3, 5, None],
-                  "min_samples_leaf": [1, 3, 5],
-                  "bootstrap": [True, False],
-                  "criterion": ["gini", "entropy"],
-                  "n_jobs": [-1]
-                  }
-
-    grid_search = GridSearchCV(clf, param_grid=param_grid)
-    grid_search.fit(X_train, y_train)
-    eval(grid_search.best_estimator_, y_test, grid_search.best_estimator_.predict(X_test))
+    clf.fit(X, y)
+  
+    # grid search
+#    param_grid = get_param_grid('MLP')
+#    grid_search = GridSearchCV(clf, param_grid=param_grid)
+#    grid_search.fit(X, y)
+#    clf = grid_search.best_estimator_
        
     # save the classifier
-    file_name = "../classifiers/" + pitcher_name + ".pkl"
-    joblib.dump(grid_search.best_estimator_, file_name)
+    file_name = "../classifiers/extended/" + pitcher_name + ".pkl"
+    joblib.dump(clf, file_name)
+
 
 def predict(data, pitcher_name):
-    file_name = "../classifiers/" + pitcher_name + ".pkl"
+    X = data[0]
+    y = data[1]
+    file_name = "../classifiers/extended/" + pitcher_name + ".pkl"
     clf = joblib.load(file_name)
-    X_train, X_test, y_train, y_test = train_test_split(data[0], data[1], test_size=0.5)
-    predictions = clf.predict(X_test)
-    eval(clf, y_test, predictions)
-    write_results(y_test, predictions, pitcher_name)
+    predictions = clf.predict(X)
+    eval(clf, y, predictions)
+    write_results(y, predictions, pitcher_name)
 
-def scores(gs, X_train, y_train)
+
+def get_param_grid(model):
+    if (model == 'MLP'):
+      param_grid = {'hidden_layer_sizes': [(10, ), (25, ), (50, ), (75, ), (100, )],
+                    'activation' : ['relu', 'logistic', 'tanh', 'identity'],
+                    'solver' : ['lbfgs', 'sgd', 'adam'],
+                    'alpha' : [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1],
+                    'batch_size' : [200,400,600,800,1000],                    
+                    }
+    if (model == 'RandomForest'):
+      param_grid = {'n_estimators': [10, 500],
+                    'max_features': ['auto', 'sqrt', 'log2'],
+                    "max_depth": [3, 5, None],
+                    "min_samples_leaf": [1, 3, 5],
+                    "bootstrap": [True, False],
+                    "criterion": ["gini", "entropy"],
+                    "n_jobs": [-1]
+                    }
+    return param_grid
+
+
+def scores(gs, X_train, y_train):
     scores = cross_val_score(gs, X_train, y_train, scoring='accuracy', cv=2)
     print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+
 
 def eval(clf, act, pred):
     print("Classification report for classifier %s:\n%s\n"
@@ -106,11 +119,12 @@ def eval(clf, act, pred):
 
 
 def write_results(act, pred, pitcher_name):
-    with open("results.txt", "a") as f:
+    with open("../classifiers/extended/nn-results.txt", "a") as f:
       f.write(pitcher_name + ":\n")
       f.write("----------------------------------------------------\n")
       f.write(metrics.classification_report(act,pred))
       f.write("\n\n")
+
 
 if __name__ == "__main__":
     main()
